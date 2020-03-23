0b547f5d0281d53670c398312d9d93b4b106bcf2
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2020-03-23 12:46:05 -0600

    Update

--- public/scripts/clientside.js
+++ public/scripts/clientside.js
@@ -136,6 +136,31 @@ function loadDefaults() {
 
     $.get("/getRecent", function(data){
         console.log(data);
+
+        document.getElementById("recentContainer").innerHTML = "";
+
+        // set number of books to display
+        if (data.list.length === 1){
+            document.getElementById("recentContainer").style.gridTemplateColumns = "1fr";
+        }
+        else if (data.list.length === 2){
+            document.getElementById("recentContainer").style.gridTemplateColumns = "1fr 1fr";
+        }
+        else if (data.list.length === 3){
+            document.getElementById("recentContainer").style.gridTemplateColumns = "1fr 1fr 1fr";
+        }
+        else if (data.list.length === 4){
+            document.getElementById("recentContainer").style.gridTemplateColumns = "1fr 1fr 1fr";
+        }
+
+        for (var i = 0; i < data.list.length; i++){
+            document.getElementById("recentContainer").innerHTML += 
+                                        '<div class="recBook">' +
+                                        '   <img src="' + data.list[i].cover_url + '" alt="book cover">' +
+                                        '   <h3>' + data.list[i].title + '</h3>' +
+                                        '   <h4>' + data.list[i].name + '</h4>' +
+                                        '</div>';
+        }
     });
 }
 
