09eead3bb7d1ca04ce1d0efcd1ce0ddeabb36b32
Nick Hendricks <hendricksn@byui.edu>
2020-04-03 14:24:09 -0600

    Project Update

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set("views", "views");
 app.set("view engine", "ejs");
+
 app.get("/auth_config.json", (req, res) => {
     res.sendFile(join(__dirname, "auth_config.json"));
   });
